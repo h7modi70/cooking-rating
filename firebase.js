2c0d79cd0b5a8f3f05312d4773e2fec3ce13c3fb
Mohameed Almarhoon <h7modi77@gmail.com>
2026-02-20 13:58:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,27 @@
+// استيراد Firebase App و Firestore
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-getFirestore,
-doc,
-setDoc,
-getDocs,
-collection
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDocs, collection } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// بيانات Firebase الخاصة بك
 const firebaseConfig = {
-apiKey:"YOUR",
-authDomain:"YOUR",
-projectId:"YOUR",
-appId:"YOUR"
+  apiKey: "AIzaSyA2LiUMrxi3T_hZCIkGVNWcVKLe0GRkAos",
+  authDomain: "cooking-rating.firebaseapp.com",
+  projectId: "cooking-rating",
+  storageBucket: "cooking-rating.firebasestorage.app",
+  messagingSenderId: "605149100590",
+  appId: "1:605149100590:web:04da9b33ca6412e6323dbf",
+  measurementId: "G-VSMW8TET7N"
 };
 
+// تهيئة Firebase
 const app = initializeApp(firebaseConfig);
+
+// تهيئة Firestore
 const db = getFirestore(app);
 
-window.db=db;
-window.setDoc=setDoc;
-window.doc=doc;
-window.getDocs=getDocs;
-window.collection=collection;
+// نجعلها متاحة في script.js
+window.db = db;
+window.setDoc = setDoc;
+window.doc = doc;
+window.getDocs = getDocs;
+window.collection = collection;
